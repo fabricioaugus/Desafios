0a899bc84c64a0cfedf6ef178e4012e4a4ca3453
fabricioaugus <49557503+fabricioaugus@users.noreply.github.com>
2021-11-18 20:03:26 -0300

    s

--- BEE_Java/src/BEE/BEE1000.java
+++ BEE_Java/src/BEE/BEE1000.java
@@ -1,9 +1,7 @@
 package bee;
+
 public class BEE1000 {
-
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
-
-	}
-
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
 }
---
+++ BEE_Java/src/BEE/BEE1001.java
@@ -0,0 +1,14 @@
+package bee;
+
+import java.util.Scanner;
+
+public class BEE1001 {
+    public static void main(String[] args) {
+        Scanner n = new Scanner(System.in);
+        int n1 = n.nextInt();
+        int n2 = n.nextInt();
+        int soma = n1+n2;
+        System.out.println("X = "+soma);
+
+    }
+}
