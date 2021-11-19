23f97cbfaf1d9f8f3f76c1bd21be2b16df88f324
fabricioaugus <49557503+fabricioaugus@users.noreply.github.com>
2021-11-19 08:33:42 -0300

    pasta hackerrank

---
+++ hackerrank_challenges/hackerrank/src/hackerrank/BasicJava.java
@@ -0,0 +1,17 @@
+package hackerrank;
+
+import java.util.*;
+
+public class BasicJava {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int numero1 = scan.nextInt();
+		int numero2 = scan.nextInt();
+		int numero3 = scan.nextInt();
+		scan.close();
+		System.out.println(numero1);
+		System.out.println(numero2);
+		System.out.println(numero3);
+	}
+}
