de8e9984542fe23650ed0d14bcefbdce7799b176
fabricioaugus <49557503+fabricioaugus@users.noreply.github.com>
2022-03-04 15:28:10 -0300

    inicio em C#

---
+++ PrimeiroAppConsole/PrimeiroAppConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace PrimeiroAppConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            int idade;
+            Console.WriteLine("Digite sua Nome:");
+            nome = Console.ReadLine();
+            
+            Console.WriteLine("Digite sua idade:");
+            idade = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine(" Olá eu sou {0} e tenho {1} anos ", nome, idade);
+
+
+        }
+
+    }
+    
+}
