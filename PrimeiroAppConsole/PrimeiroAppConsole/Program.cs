7565ad301a0b9fbe58e36404165c7357729088ed
fabricioaugus <49557503+fabricioaugus@users.noreply.github.com>
2022-03-07 13:37:27 -0300

    exercicios 1,2 e 3

--- PrimeiroAppConsole/PrimeiroAppConsole/Program.cs
+++ PrimeiroAppConsole/PrimeiroAppConsole/Program.cs
@@ -5,20 +5,95 @@ namespace PrimeiroAppConsole
     {
         static void Main(string[] args)
         {
-            string nome;
-            int idade;
-            Console.WriteLine("Digite sua Nome:");
-            nome = Console.ReadLine();
-            
-            Console.WriteLine("Digite sua idade:");
-            idade = Convert.ToInt32(Console.ReadLine());
-
-
-            Console.WriteLine(" Olá eu sou {0} e tenho {1} anos ", nome, idade);
-
+            while (true)
+            {
+                menu();
+                int opcao = Convert.ToByte(Console.ReadLine());
+                switch (opcao)
+                {
+                    case 1:
+                        verificaParOuImpar();
+                        break;
+                    case 2:
+                        verificaIdade();
+                        break;
+                    case 3:
+                        calculaTabuadaN();
+                        break;
+                } if (opcao == 0) {
+                    break;
+                }
+            }
+            Console.WriteLine("FECHOU O PROGRAMA !!!");
 
         }
+        /* Método - Exercicio 1
+      * Desenvolver um algoritmo console que solicite entrada de 
+      * um número e ver se é par or impar
+       */
+        private static void verificaParOuImpar()
+        {
+            {
 
+                int nPar;
+                Console.WriteLine("Digite um numero para ver se ele é par ou impar: ");
+                nPar = Convert.ToInt32(Console.ReadLine());
+
+                if (nPar % 2 == 0) Console.WriteLine(" O numero é Par: {0} ", nPar);
+
+                else Console.WriteLine(" O numero é impar: {0}", nPar);
+
+
+            }
+        }
+        /* Método - Exercicio 2
+        *Desenvolva um algoritmo que solicite a entrada da idade de um determinado usuário,
+        *se for menor que 18 anos exiba na cor vermelha “Sem permissão”, caso seja maior ou  igual a 18 anos exiba
+        *na cor verde “Permissão concedida”.
+       */
+
+        private static void verificaIdade()
+        {
+            int idade;
+            string corVermelha = "Sem Permissão";
+            string corVerde = "Permissão Concedida";
+
+            Console.WriteLine("Digite a sua idade, você pode ou não ter permissão:");
+
+            idade = Convert.ToInt32(Console.ReadLine());
+
+            if (idade >= 18) Console.WriteLine(corVerde);
+            else Console.WriteLine(corVermelha);
+
+        }
+        /* Método - Exercicio 3
+             Leia 1 valor inteiro "N" (entre 2 e 1.000), e a seguir mostre a tabuada de "N".
+         * Exemplo: 1 X N = N | 2 x N = 2N ….
+        */
+        private static void calculaTabuadaN()
+        {
+
+            Console.WriteLine("digite um numero para retornar a tabuada Dele : ");
+            int numeroN = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                int tabuada = i * numeroN;
+                Console.WriteLine("{0} X {1} = {2}", i, numeroN, tabuada);
+            }
+
+        }
+       // menu de opcões 
+        private static void menu()
+        {
+            Console.WriteLine("escolha uma das opceos abaixo: ");
+            Console.WriteLine("1 - Ver se o numero é Par ou Impar ");
+            Console.WriteLine("2 - Verificar se você é maior de idade ");
+            Console.WriteLine("3 - Tabuada de um numero N");
+
+            Console.WriteLine("0 - Sair do programa");
+
+        }
     }
-    
 }
+
